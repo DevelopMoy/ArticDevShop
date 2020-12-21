948df8956e029b6327e21fb625e5bdf52b92a1c6
moy <fabmoy1866@gmail.com>
2020-12-21 03:53:10 -0600

    Prueba enviar email recuperar clave 2

--- appweb/mod/recuperLogica.php
+++ appweb/mod/recuperLogica.php
@@ -15,19 +15,7 @@ if(isset($_POST["emailRec"])&&!empty($_POST["emailRec"])){
                 $to=$_POST["emailRec"];
                 $subject = "Recuperar Contraseña Artic Dev";
                 $message='
-                    <html>
-                    <head>
-                      <title>Recupere su Contraseña</title>
-                    </head>
-                    <body>
-                        <img src="https://articdev.online/appweb/images/logoForEmail.jpeg">
-                      <h1 style="font-size: 26px; color: black;">Estimado Cliente, Favor de seguir las instrucciones para recuperar su cuenta, si no lo ha solicitado usted, pongase en contacto con nosotros</h1>
-                      <p>Enviado el dia '.date("F j, Y, g:i a").'</p>
-                      <p style="font-size: 24px; color: #004481;">Ingrese al siguiente enlace y escriba el codigo: </p>
-                      <p style="color: #00b7ff; font-size: 30px; margin: 5px; border: 1px solid black; border-radius: 5px; width: 25%;">'.$codigoRecu.'</p>
-                      <a href="https://articdev.online/appweb/mod/recuUsuar.php" style="font-size: 22px; color: #004481;">Presione aquí</a>
-                    </body>
-                    </html>
+                    hola
                     ';
                 $headers="From: ".$from."\r\n";
                 $headers.='Content-type: text/html; charset=iso-8859-1' . "\r\n";
@@ -35,10 +23,6 @@ if(isset($_POST["emailRec"])&&!empty($_POST["emailRec"])){
             }else{
                 header("Location: recuperar.php?state=failbd");
             }
-
-
-
-
             header("Location: recuperar.php?state=success");
         }else{
 
