5a5b0bee03a5561224f0e7a39169515c50441377
moy <fabmoy1866@gmail.com>
2020-12-21 04:31:01 -0600

    Prueba enviar email recuperar clave 3

--- appweb/mod/recuperLogica.php
+++ appweb/mod/recuperLogica.php
@@ -22,18 +22,18 @@ if(isset($_POST["emailRec"])&&!empty($_POST["emailRec"])){
                 $to=$_POST["emailRec"];
                 $subject = "Recuperar Artic Dev";
                 $message='
-                    <html>
-                    <head>
-                   
-                    </head>
-                    <body>
-                        <img src="https://articdev.online/appweb/images/logoForEmail.jpeg">
-                          <h1 style="font-size: 26px; color: black;">Estimado Cliente, Favor de seguir las instrucciones para recuperar su cuenta, si no lo ha solicitado usted, pongase en contacto con nosotros</h1>
-                          <p style="font-size: 24px; color: #004481;">Ingrese al siguiente enlace y escriba el codigo: </p>
-                          <p style="color: #00b7ff; font-size: 30px; margin: 5px; border: 1px solid black; border-radius: 5px; width: 25%;">'.strval($codigoRecu).'</p>
-                          <a href="https://articdev.online/appweb/mod/recuUsuar.php" style="font-size: 22px; color: #004481;">Presione aquí</a>
-                    </body>
-                    </html>
+                         <html>
+                        <head>
+                          <title>Mensaje Cliente</title>
+                        </head>
+                        <body>
+                          <h1>Recupera tu contraseña</h1>
+                          <p>Estimado Cliente, Favor de seguir las instrucciones para recuperar su cuenta, si no lo ha solicitado usted, pongase en contacto con nosotros</p>
+                          <p>Ingrese al siguiente enlace y escriba el codigo: </p>
+                          <p>'.strval($codigoRecu).'</p>
+                          <a href="https://articdev.online/appweb/mod/recuUsuar.php">Presione aquí</a>
+                        </body>
+                        </html>
                     ';
                 $headers="From: ".$from."\r\n";
                 $headers.='Content-type: text/html; charset=iso-8859-1' . "\r\n";
